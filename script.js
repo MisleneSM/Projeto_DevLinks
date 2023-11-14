3a292f146240274c9fcd3908bb4c8ef389b415f3
Mislene Silva Moura <mislene.moura2000@gmail.com>
2023-11-14 19:42:55 -0300

    Ajustes finais

--- script.js
+++ script.js
@@ -6,8 +6,8 @@ function toggleMode() {
     const img = document.querySelector('#profile img')
 
     if(html.classList.contains('ligth')){
-        img.setAttribute('src', './assets/Avatar-ligth.png')
+        img.setAttribute('src', './assets/avatarLigth.png')
     }else {
-        img.setAttribute('src', './assets/Avatar.png')
+        img.setAttribute('src', './assets/avatar.png')
     }
 }
